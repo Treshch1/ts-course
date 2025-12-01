8cf76952a8549b0f5ea4183d778731ae2afa4244
Treshch1 <vladislavtreshcheyko@gmail.com>
2025-12-01 22:56:33 +0200

    Lesson-21

---
+++ lesson-21/fixtures/app-fixture.ts
@@ -0,0 +1,17 @@
+import { test as base, Page } from '@playwright/test';
+import { AppPage } from 'pom/app.pom';
+
+export const test = base.extend<{ app: AppPage }>({
+    app: async ({ page }: { page: Page }, use) => {
+        const app = new AppPage(page);
+        await app.homepage.goto();
+        try {
+            // Sometimes a modal appears on homepage load
+            await app.modalCloseButton.waitFor({ state: 'visible', timeout: 2000 });
+            await app.modalCloseButton.click();
+        } catch {
+            // Ignore if modal is not present
+        }
+        await use(app);
+    }
+});
---
+++ lesson-21/readme.md
@@ -0,0 +1,24 @@
+# Lesson 21
+
+In this lesson we explored reports.
+
+
+## Getting Started
+
+To install the necessary dependencies, run:
+```
+npm i
+```
+
+
+To run the tests, you can use Node.js.
+```
+npm run test
+```
+
+To see allure report install allure and run:
+```
+allure serve allure-results
+```
+
+To see mochawesome report, open the `mochawesome-report/mochawesome.html` file in your browser.
